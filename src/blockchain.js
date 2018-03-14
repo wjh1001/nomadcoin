dd0f14ca865a6bc14662b67670c9556210fc9fa9
Andy Woo <cheese.wo@gmail.com>
2018-03-14 18:23:47 +0900

    #1-6. createHash and createNewBlock function part 2

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,3 +1,5 @@
+const CryptoJS = require("crypto-js");
+
 class Block{
     constructor(index, hash, previousHash, timestamp, data){
         this.index = index;
@@ -24,11 +26,28 @@ const getLastBlock = () => blockchain[blockchain.length - 1];
 
 const getTimestamp = () => new Date().getTime() / 1000;
 
+const createHash = (index, hash, previousHash, timestamp, data) =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+
 const createNewBlock = data => {
     const previousBlock = getLastBlock();
     const newBlockIndex = previousBlock.index + 1;
     const newTimestamp = getTimestamp();
-
-}
+    const newHash = createHash( //create new Hash
+        newBlockIndex, 
+        previousBlock.hash,
+        newTimestamp,
+        data
+    );
+    const newBlock = new Block(  //create new Block
+        newBlockIndex,
+        newHash,
+        previousBlock.hash,
+        newTimestamp,
+        data
+    );
+    return newBlock;
+};
 
 console.log(blockchain);
