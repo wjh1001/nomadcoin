2c4913a6ca16bf4475290e7159641c855deb89bc
Andy Woo <cheese.wo@gmail.com>
2018-03-15 12:34:27 +0900

    doing

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express"),
     bodyParser = require("body-parser"),
     morgan = require("morgan"),
-    Blockchain = require("./blockchain");
+    Blockchain = require("./blockchain"),
+    P2P = require("./p2p");
 
 
 const { getBlockchain, createNewBlock } = Blockchain;
+const { startP2PServer } = P2P;
 
 const PORT = process.env.HTTP_PORT || 3000;
 
