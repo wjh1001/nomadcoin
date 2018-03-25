f02e1cb19f050df1a26619c1f4ea2af90e0b353f
Andy Woo <cheese.wo@gmail.com>
2018-03-25 10:53:13 +0900

    fix eror: hexToBinary -> hashToBinary

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,130 +1,161 @@
-const CryptoJS = require('crypto-js'),
-  hexToBinary = require('hex-to-binary')
+const CryptoJS = require("crypto-js"),
+  hexToBinary = require("hex-to-binary");
 
 class Block {
-  constructor (index, hash, previousHash, timestamp, data, difficulty, nonce) {
-    this.index = index
-    this.hash = hash
-    this.previousHash = previousHash
-    this.timestamp = timestamp
-    this.data = data
-    this.nonce = nonce
-    this.difficulty = difficulty
+  constructor(index, hash, previousHash, timestamp, data, difficulty, nonce) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.difficulty = difficulty;
+    this.nonce = nonce;
   }
 }
 
 const genesisBlock = new Block(
-	0,
-	'2C4CEB90344F20CC4C77D626247AED3ED530C1AEE3E6E85AD494498B17414CAC',
-	null,
-	1520312194926,
-	'This is the genesis!!',
-	0,
-	0
-)
+  0,
+  "2C4CEB90344F20CC4C77D626247AED3ED530C1AEE3E6E85AD494498B17414CAC",
+  null,
+  1520312194926,
+  "This is the genesis!!",
+  0,
+  0
+);
 
-let blockchain = [genesisBlock]
+let blockchain = [genesisBlock];
 
-const getNewestBlock = () => blockchain[blockchain.length - 1]
+const getNewestBlock = () => blockchain[blockchain.length - 1];
 
-const getTimestamp = () => new Date().getTime() / 1000
+const getTimestamp = () => new Date().getTime() / 1000;
 
-const getBlockchain = () => blockchain
+const getBlockchain = () => blockchain;
 
 const createHash = (index, previousHash, timestamp, data, difficulty, nonce) =>
-	CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data) + difficulty + nonce).toString()
+  CryptoJS.SHA256(
+    index + previousHash + timestamp + JSON.stringify(data) + difficulty + nonce
+  ).toString();
 
 const createNewBlock = data => {
-  const previousBlock = getNewestBlock()
-  const newBlockIndex = previousBlock.index + 1
-  const newTimestamp = getTimestamp()
-  const newBlock = findBlock(newBlockIndex, previousBlock.hash, newTimestamp, data, 5)
-  addBlockToChain(newBlock)
-  require('./p2p').broadcastNewBlock()
-  return newBlock
-}
+  const previousBlock = getNewestBlock();
+  const newBlockIndex = previousBlock.index + 1;
+  const newTimestamp = getTimestamp();
+  const newBlock = findBlock(
+    newBlockIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data,
+    20
+  );
+  addBlockToChain(newBlock);
+  require("./p2p").broadcastNewBlock();
+  return newBlock;
+};
 
 const findBlock = (index, previousHash, timestamp, data, difficulty) => {
-  let nonce = 0
+  let nonce = 0;
   while (true) {
-    const hash = createHash(index, previousHash, timestamp, data, difficulty, nonce)
-		// to do: check amount of zeros (hashMatchesDifficult)
+    console.log("Current nonce", nonce);
+    const hash = createHash(
+      index,
+      previousHash,
+      timestamp,
+      data,
+      difficulty,
+      nonce
+    );
     if (hashMatchesDifficulty(hash, difficulty)) {
-      return new Block(index, hash, previousHash, timestamp, data, difficulty, nonce)
+      return new Block(
+        index,
+        hash,
+        previousHash,
+        timestamp,
+        data,
+        difficulty,
+        nonce
+      );
     }
-    nonce++
+    nonce++;
   }
-}
+};
 
 const hashMatchesDifficulty = (hash, difficulty) => {
-  const hexInBinary = hexToBinary(hash)
-  const requiredZeros = '0'.repeat(difficulty)
-  console.log('Trying difficulty:', difficulty, 'with hash:', hash)
-  return hashInBinary.startsWith(requiredZeros)
-}
+  const hashInBinary = hexToBinary(hash);
+  const requiredZeros = "0".repeat(difficulty);
+  console.log("Trying difficulty:", difficulty, "with hash", hashInBinary);
+  return hashInBinary.startsWith(requiredZeros);
+};
 
-const getBlocksHash = block => createHash(block.index, block.previousHash, block.timestamp, block.data)
+const getBlocksHash = block =>
+  createHash(block.index, block.previousHash, block.timestamp, block.data);
 
 const isBlockValid = (candidateBlock, latestBlock) => {
   if (!isBlockStructureValid(candidateBlock)) {
-    console.log('The candidate block structure is not valid')
-    return false
+    console.log("The candidate block structure is not valid");
+    return false;
   } else if (latestBlock.index + 1 !== candidateBlock.index) {
-    console.log('The candidate block doesnt have a valid index')
-    return false
+    console.log("The candidate block doesnt have a valid index");
+    return false;
   } else if (latestBlock.hash !== candidateBlock.previousHash) {
-    console.log('The previousHash of the candidate block is not the hash of the latest block')
-    return false
+    console.log(
+      "The previousHash of the candidate block is not the hash of the latest block"
+    );
+    return false;
   } else if (getBlocksHash(candidateBlock) !== candidateBlock.hash) {
-    console.log('The hash of this block is invalid')
-    return false
+    console.log("The hash of this block is invalid");
+    return false;
   }
-  return true
-}
+  return true;
+};
 
 const isBlockStructureValid = block => {
   return (
-		typeof block.index === 'number' &&
-		typeof block.hash === 'string' &&
-		typeof block.previousHash === 'string' &&
-		typeof block.timestamp === 'number' &&
-		typeof block.data === 'string'
-  )
-}
+    typeof block.index === "number" &&
+    typeof block.hash === "string" &&
+    typeof block.previousHash === "string" &&
+    typeof block.timestamp === "number" &&
+    typeof block.data === "string"
+  );
+};
 
 const isChainValid = candidateChain => {
   const isGenesisValid = block => {
-    return JSON.stringify(block) === JSON.stringify(genesisBlock)
-  }
+    return JSON.stringify(block) === JSON.stringify(genesisBlock);
+  };
   if (!isGenesisValid(candidateChain[0])) {
-    console.log("The candidateChains's genesisBlock is not the same as our genesisBlock")
-    return false
+    console.log(
+      "The candidateChains's genesisBlock is not the same as our genesisBlock"
+    );
+    return false;
   }
   for (let i = 1; i < candidateChain.length; i++) {
     if (!isBlockValid(candidateChain[i], candidateChain[i - 1])) {
-      return false
+      return false;
     }
   }
-  return true
-}
+  return true;
+};
 
 const replaceChain = candidateChain => {
-  if (isChainValid(candidateChain) && candidateChain.length > getBlockchain().length) {
-    blockchain = candidateChain
-    return true
+  if (
+    isChainValid(candidateChain) &&
+    candidateChain.length > getBlockchain().length
+  ) {
+    blockchain = candidateChain;
+    return true;
   } else {
-    return false
+    return false;
   }
-}
+};
 
 const addBlockToChain = candidateBlock => {
   if (isBlockValid(candidateBlock, getNewestBlock())) {
-    blockchain.push(candidateBlock)
-    return true
+    blockchain.push(candidateBlock);
+    return true;
   } else {
-    return false
+    return false;
   }
-}
+};
 
 module.exports = {
   getNewestBlock,
@@ -133,4 +164,4 @@ module.exports = {
   isBlockStructureValid,
   addBlockToChain,
   replaceChain
-}
+};
